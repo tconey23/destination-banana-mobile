073def14cb436f4f92af961bff4a1ca12e8d59dd
Adam Bedient <adambedient@gmail.com>
2024-08-20 15:33:10 -0400

    Initial commit

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Pressable } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Destination: Banana Home Page</Text>
+      <Pressable></Pressable>
       <StatusBar style="auto" />
     </View>
   );
