1587de4a8d1e3bc77186c16daf05936bc5679ee0
Adam Bedient <adambedient@gmail.com>
2024-08-21 15:41:36 -0400

    Define buttons

--- App.js
+++ App.js
@@ -1,21 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Pressable } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, Text, View, Pressable } from 'react-native'
+import LandingButton from './components/LandingButton'
 
-export default function App() {
+function App() {
+
+  function startGame() {
+
+  }
+
+  function showInstructions() {
+
+  }
+
+
+
   return (
     <View style={styles.container}>
-      <Text>Destination: Banana Home Page</Text>
-      <Pressable></Pressable>
+      <Text style={styles.text}>Destination: Banana Home Page</Text>
+      <View style={styles.buttonContainer}>
+        <LandingButton onButtonPress={startGame} color="black"/>
+        <LandingButton onButtonPress={showInstructions} color="red"/>
+      </View>
       <StatusBar style="auto" />
     </View>
-  );
+  )
 }
+
+export default App
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: 'column',
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+  text: {
+    textAlign: 'center',
+  },
+  buttonContainer: {
+    height: 300,
+    width: 300,
+    backgroundColor: 'pink'
+  }
+})
